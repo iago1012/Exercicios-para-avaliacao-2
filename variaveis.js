098ad2f9a1944987647ddadec8bda3a7bc7e9950
iago1012 <iago_maciel@uniplaclages.edu.br>
2025-07-02 18:54:04 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+let nome = "Iago";       // String
+let idade = 18;          // Inteiro
+let altura = 1.75;       // Float
+let ativo = true;        // Booleano
+
+alert("Nome: " + nome);
+alert("Idade: " + idade);
+alert("Altura: " + altura);
+alert("Está ativo? " + ativo);
